04ac1ecd33011735feb3c9ea274c4e820f8e1532
Pragya Gupta <guptapragya9026@gmail.com>
2023-02-27 11:10:54 +0530

    func2

--- game1.py
+++ game1.py
@@ -1,13 +1,23 @@
 import pgzrun
 
-b = Rect ((50,50) , (100,50))
-vx = 1 #global variables
+# music bgm music
+ #music.play('bg)
+
+b = Rect((50,50) , (100,50))
+vx,vy = 1, 1
 
 def draw():
     screen.fill('black')
     screen.draw.filled_rect(b,'blue')
 
 def update():
+    global vx,vy 
     b.x += vx
+    b.y += vy
+    if b.right > 800 or b.left < 0:
+        vx = -vx
+    if b.bottom > 600 or b.top < 0:
+        vx = -vy
 
-pgzrun.go()                        
+# outside of all function
+pgzrun.go()                                   
